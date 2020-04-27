cd40089db2be4ada056a3269fb6d918221b355d8
Timo <timo.schessl@gmx.de>
2020-04-27 22:41:53 +0200

    Defined two functions for getting a specific row or a specific column

--- mysqlApp.py
+++ mysqlApp.py
@@ -4,11 +4,21 @@ database = mysql.connector.connect(host="localhost",user="root",passwd="",databa
 
 cursor = database.cursor()
 
-cursor.execute("select * from user")
 
-for i in cursor:
-    print(i)
+def getRowOfUserTable(row):
+    cursor.execute("select * from user")
+    result= cursor.fetchall()
 
+    print(result[row])
+
+
+def getColumnOfUserTable(columnName):
+    cursor.execute("select "+columnName+" from user")
+
+    for i in cursor:
+        print(i)
+
+getColumnOfUserTable("username")
 
 
 
